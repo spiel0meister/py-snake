c8f2ec7d6e47a4ea194f8d6d0abc299799d3a182
spiel0meister <soviczan7@gmail.com>
2022-11-12 12:32:33 +0100

    added a snake and drawing it

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+from itertools import product
 import pygame
+from random import randint
 
 WIDTH, HEIGHT = 400, 400
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+ROWS, COLS = 10, 10
+CELL_WIDTH, CELL_HEIGHT = WIDTH / COLS, HEIGHT / ROWS
+
 black = (0, 0, 0)
+red = (255, 0, 0)
+yellow = (255, 255, 0)
+
+
+class Pos:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+snake = [Pos(randint(0, COLS-1), randint(0, ROWS-1))]
+
 
 def draw():
     WIN.fill(black)
+
+    for pos in snake:
+        pygame.draw.rect(WIN, red, (pos.x * CELL_WIDTH,
+                         pos.y * CELL_HEIGHT, CELL_WIDTH - 1, CELL_HEIGHT - 1))
+
     pygame.display.update()
 
 
