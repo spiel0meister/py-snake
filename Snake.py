8c235bd9a5a196b766e9e43b08f94b90ffd4c83f
spiel0meister <soviczan7@gmail.com>
2022-11-12 14:17:04 +0100

    turned snake into a Snake object and much more
    Added the Snake class and turned the snake into a snake object.
    
    Also added:
    - eating food
    - moving the snake
    - updating the snake's body

---
+++ Snake.py
@@ -0,0 +1,70 @@
+from random import randint
+from typing import Tuple
+
+
+class Pos:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def create_random_pos(x_range: Tuple[int, int], y_range: Tuple[int, int]):
+        return Pos(randint(x_range[0], x_range[1]), randint(y_range[0], y_range[1]))
+
+    def __sub__(self, other):
+        return Pos(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, num):
+        return Pos(self.x * num, self.y * num)
+
+    def __str__(self): return f"({self.x}, {self.y})"
+
+
+class Snake:
+    def __init__(self, head_x: int, head_y: int):
+        self.body = [Pos(head_x, head_y)]
+        self.state = None
+
+    def draw(self, pygame, WIN, color, W, H):
+        for pos in self.body:
+            pygame.draw.rect(WIN, color, (pos.x * W,
+                                          pos.y * H, W - 1, H - 1))
+
+    def update(self):
+        i = len(self.body) - 1
+        while i > 0:
+            self.body[i].x = self.body[i-1].x
+            self.body[i].y = self.body[i-1].y
+            i -= 1
+        state = self.state
+        if state == 0:
+            self.head.y -= 1
+        elif state == 1:
+            self.head.x += 1
+        elif state == 2:
+            self.head.y += 1
+        elif state == 3:
+            self.head.x -= 1
+
+    def pick_up_food(self, food):
+        if food.x != self.head.x or food.y != self.head.y:
+            return False
+        if len(self.body) == 1:
+            state = self.state
+            if state == 0:
+                self.body.append(Pos(self.head.x, self.head.y+1))
+            elif state == 1:
+                self.body.append(Pos(self.head.x - 1, self.head.y))
+            elif state == 2:
+                self.body.append(Pos(self.head.x, self.head.y-1))
+            elif state == 3:
+                self.body.append(Pos(self.head.x + 1, self.head.y))
+        else:
+            p1 = self.body[len(self.body) - 1]
+            p2 = self.body[len(self.body) - 2]
+            p_new = p1 * 2 - p2
+            self.body.append(p_new)
+        return True
+
+    @property
+    def head(self) -> Pos: return self.body[0]
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from itertools import product
 from random import randint
-from typing import Tuple
 import pygame
+import Snake
 
 WIDTH, HEIGHT = 400, 400
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+CLOCK = pygame.time.Clock()
 
 ROWS, COLS = 10, 10
 CELL_WIDTH, CELL_HEIGHT = WIDTH / COLS, HEIGHT / ROWS
@@ -14,42 +14,63 @@ red = (255, 0, 0)
 yellow = (255, 255, 0)
 
 
-class Pos:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+def create_food(snake_body):
+    random_pos = None
+    free = False
+    while not free:
+        random_pos = Snake.Pos.create_random_pos((0, COLS-1), (0, ROWS-1))
+        free = True
+        for pos in snake_body:
+            if pos.x == random_pos.x and pos.y == random_pos.y:
+                random_pos = Snake.Pos.create_random_pos(
+                    (0, COLS-1), (0, ROWS-1))
+                free = False
+    return random_pos
 
-    @staticmethod
-    def create_random_pos(x_range: Tuple[int, int], y_range: Tuple[int, int]):
-        return Pos(randint(x_range[0], x_range[1]), randint(y_range[0], y_range[1]))
+
+# STATES:
+#    0: UP
+#    1: RIGHT
+#    2: DOWN
+#    3: LEFT
 
 
-snake = [Pos.create_random_pos((0, COLS-1), (0, ROWS-1))]
-food = Pos.create_random_pos((0, COLS-1), (0, ROWS-1))
-
-
-def draw():
+def draw(snake, food):
     WIN.fill(black)
-
-    for pos in snake:
-        pygame.draw.rect(WIN, red, (pos.x * CELL_WIDTH,
-                         pos.y * CELL_HEIGHT, CELL_WIDTH - 1, CELL_HEIGHT - 1))
 
     pygame.draw.rect(WIN, yellow, (food.x * CELL_WIDTH,
                                    food.y * CELL_HEIGHT, CELL_WIDTH - 1, CELL_HEIGHT - 1))
+
+    snake.draw(pygame, WIN, red, CELL_WIDTH, CELL_HEIGHT)
 
     pygame.display.update()
 
 
 def while_loop():
+    snake = Snake.Snake(randint(0, COLS-1), randint(0, ROWS-1))
+    food = create_food(snake.body)
+
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    snake.state = 0
+                elif event.key == pygame.K_d:
+                    snake.state = 1
+                elif event.key == pygame.K_s:
+                    snake.state = 2
+                elif event.key == pygame.K_a:
+                    snake.state = 3
 
-        draw()
+        if snake.pick_up_food(food):
+            food = create_food(snake.body)
+        snake.update()
+        draw(snake, food)
+        CLOCK.tick(5)
 
 
 def main(): while_loop()
